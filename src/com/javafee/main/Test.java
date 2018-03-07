05102112f0ebd9a1d10264a3839ae85155891a71
rutko <rutko@192.168.0.122>
2018-03-07 17:22:04 +0100

    JaRutkowski Small changes

--- src/com/javafee/main/Test.java
+++ src/com/javafee/main/Test.java
@@ -1,9 +1,9 @@
 package com.javafee.main;
 
-public class Main {
+public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("I was there!");
+		System.out.println("There I was too!");
 	}
 
 }
